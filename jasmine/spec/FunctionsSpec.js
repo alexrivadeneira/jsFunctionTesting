93cea5bee47212f18a06dc19a2afaa1cbe7f52cc
alexrivadeneira <alexrivs@gmail.com>
2018-04-13 21:32:04 -0700

    init

---
+++ jasmine/spec/FunctionsSpec.js
@@ -0,0 +1,19 @@
+// DO NOT CHANGE ANY CODE BELOW IN THIS FILE
+
+describe("addTwoNums", function(){
+	it("should return the sum of two nums", function(){
+		expect(addTwoNums(2,3)).toEqual(5);
+	})
+});
+
+describe("addTwoNums", function(){
+	it("should return the sum of two nums", function(){
+		expect(addTwoNums(11,11)).toEqual(22);
+	})
+});
+
+describe("addThreeNums", function(){
+	it("should return the sum of three nums", function(){
+		expect(addTwoNums(1,1,1)).toEqual(3);
+	})
+});
---
+++ jasmine/src/Functions.js
@@ -0,0 +1,9 @@
+function addTwoNums(num1, num2){
+	var sum = num1 + num2 + 5;
+	return sum;
+}
+
+function addThreeNums(num1, num2, num3){
+	var sum = num1 + num2 + num3;
+	return sum;
+}
